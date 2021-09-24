bd608f77d384b565571a31fd47fedb607a778c5c
Andrian Melnic <melnic.andry97@gmail.com>
2021-09-24 15:54:41 +0200

    Updated truffle, solc & openzeppelin/contracts

--- truffle-config.js
+++ truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "^0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "^0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
